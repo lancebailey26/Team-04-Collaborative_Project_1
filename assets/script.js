4fc07013107ae0751f14ae80b2e8b0f69a8221e7
Lance Bailey <lancebailey222@gmail.com>
2021-06-08 18:22:59 -0400

    added some dropdown menus + used their variables in api call

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
-var input = 1; //participants or any input parameter
-var maxprice = 1; // 0.0 - 1
+
+// var input = 1; //participants or any input parameter
+ // 0.0 - 1
 
 var badweather = false; //something we can set from another api
 
+
+
+function boredAPI(){
+var participants = $("#participant option:selected").val();
+var maxprice = $("#maxprice option:selected").val();
+  console.log(participants);
 if (badweather === true){
-    fetch('http://www.boredapi.com/api/activity?&participants='+input+'&maxprice='+maxprice+'&type=education&type=relaxation&type=busywork&type=cooking&type=music&type=diy')
+    fetch('http://www.boredapi.com/api/activity?&participants='+participants+'&maxprice='+maxprice+'&type=education&type=relaxation&type=busywork&type=cooking&type=music&type=diy')
     .then(function (response) {
     return response.json();
     })
@@ -15,13 +22,11 @@ if (badweather === true){
         var price = data.price;
         $('h3').text(act);
         $('h4').text('as far as price, we rate this as a '+ price+' on a scale of 0 - 1')
-        $('p').text(participants);
+        $('p').text('this is an idea for an activity with '+participants+' participant(s). run the code again if you dont like it.');
 
     })
-    
-}
-if (badweather===false){
-fetch('http://www.boredapi.com/api/activity?&participants='+input+'&type=recreational&type=diy')
+}else{
+    fetch('http://www.boredapi.com/api/activity?&participants='+participants+'&maxprice='+maxprice)
     .then(function (response) {
     return response.json();
     })
@@ -36,3 +41,5 @@ fetch('http://www.boredapi.com/api/activity?&participants='+input+'&type=recreat
 
     })
 }
+}
+$('#go').on('click',boredAPI)
