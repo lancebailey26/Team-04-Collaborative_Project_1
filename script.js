fd646eb34b26f5c24233d0624caf3ed92c374981
Dawn Jones <dawn_jones918@yahoo.com>
2021-06-09 19:48:40 -0400

    fixed addeventlistener issue executing prior to click submit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// var startButton = document.querySelector("#submit");
+// startButton.addEventListener('click', countdown);
+
+document.getElementById("submit").addEventListener("click", countdown);
 
 
-document.getElementById("submit").addEventListener("click", countdown())
-
-
-function countdown()
+function countdown(event)
 {
+    event.preventDefault();
     var count = 4;
-
+    console.log("test");
+    console.log(count);
     var interval = setInterval(function()
     {
         document.getElementById("timer").innerHTML = "Let the boredom dissappear in... " + -- count;
