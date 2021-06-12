300fd4990d8ef346cfc399a8e9a16cf2742d7e2b
Lance Bailey <lancebailey222@gmail.com>
2021-06-12 14:26:53 -0400

    added localstorage feature

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,14 @@
 //Event listener for submit click
 document.getElementById("submit").addEventListener("click", countdown);
+localStorage.setItem('searchCounter', 0);
+
 
 //Countdown timer to display  successive user interactive pages based on weather and participant input
 function countdown(event) {
     event.preventDefault();
-    // var city = document.querySelector('#state').value;
-    // if (city == null || city == " "){
-    //     $('timer').addClass('hide;')
-    //     $('#resultsbox').removeClass('hide');
-    //      $('#weather1').text("you need to enter a location!")
-    //      return
-    // }else{
     $('#timer').removeClass('hide');
     $('#resultsbox').addClass('hide');
     var count = 4;
-    // console.log("test");
-    // console.log(count);
     var interval = setInterval(function () {
         var timerEl = document.getElementById("timer");
         timerEl.innerHTML = "Let the boredom dissappear in... " + --count;
@@ -37,14 +30,14 @@ function clear() {
     $('#activity1').text('');
     $('#activity2').text('');
     $('#activity3').text('');
+    $('#activity4').text('');
+    $('#activity5').text('');
 }
 // The function below calls the weather API ;Depending upon the city's weather conditions lists suitable random activities from the BoredAPI
 function boredAPI() {
     clear();
     var city = document.querySelector('#state').value;
     var apiKey = "6ca38b593072c5ff245976d803e5f35b";
-
-
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&units=imperial" + "&appid=" + apiKey)
         .then(function (response) {
             let data = response.json();
@@ -56,16 +49,17 @@ function boredAPI() {
         })
         .then(function (data) {
             console.log(data)
-            // var temperatureContainerEl = document.querySelector('#temperature-container');
-            // var cityContainerEl = document.querySelector('#city-container');
-            // var dateContainerEl = document.querySelector('#date-container');
-            // var nameContainerEl = document.querySelector('#name-container');
-            // var weatherDescEl=document.querySelector("#weather-description");
             var date = data.dt;
             var displayDate = moment.unix(date).format("MM/DD/YYYY");
             var citySearch = data.name;
             var temperature = data.main.temp;
             var weatherdesc = data.weather[0].main;
+            if(localStorage.getItem("searchCounter")===null){
+                localStorage.setItem("searchCounter", 0)
+            }
+            var searchCount = parseInt(localStorage.getItem("searchCounter"));
+            var plusOne = searchCount + 1;
+            localStorage.setItem("searchCounter", plusOne);
 
 
 
@@ -79,9 +73,6 @@ function boredAPI() {
             // future: could have a full page gif based on location weather (cloudy, rainy, thunderstorm, sun)
 
             var participants = $("#participants option:selected").val();
-            // var badWeather = $('#weather4').text(); 
-            // var maxprice = $("#maxprice option:selected").val();
-            // console.log(participants);
             if (weatherdesc === "Rain" || weatherdesc === "Thunderstorm" || weatherdesc === "Drizzle" || weatherdesc === "Snow" || temperature < 50) {
                 fetch('http://www.boredapi.com/api/activity?&participants=' + participants + '&type=education&type=relaxation&type=music&type=cooking')
                     .then(function (response) {
@@ -97,7 +88,7 @@ function boredAPI() {
                         $('#activity2').text("Price Scale: " + price + " (scale of 0 - 1)")
                         $('#activity3').text("Participants: " + participants);
                         $('#activity4').text("Complete the form again for more ideas!");
-
+                        $('#activity5').text("You've searched "+ localStorage.getItem('searchCounter') +" times. Are you bored yet?")
 
                     })
             } else {
@@ -115,77 +106,10 @@ function boredAPI() {
                         $('#activity2').text("Price Scale: " + price + " (scale of 0 - 1)")
                         $('#activity3').text("Participants: " + participants);
                         $('#activity4').text("Complete the form again for more ideas!");
+                        $('#activity5').text("You've searched "+plusOne+" times. Are you bored yet?")
 
                     })
             }
-        })
+            }
+        )
 }
-
-
-
-
-
-            // var cityButton = document.createElement('button');
-            // cityButton.innerHTML = citySearch;
-
-            //  weatherDescEl.innerHTML = weatherdesc;
-            // dateContainerEl.innerHTML = displayDate;
-            // nameContainerEl.innerHTML = citySearch;
-            // temperatureContainerEl.innerHTML = "Temperature: " + temperature + "°F";
-            // var weatherDisplay = $('#weather4').text();
-            // var tempDisplay = $('#weather3').val();
-            // if (weatherDisplay == "rain"){
-            //     badWeather = true;
-            //    }else{
-            //      badWeather = false;
-            //    }
-
-
-
-
-        //if #weather4 !== rain || if #weather3 < 50
-        //then badweather == false
-
-
-
-
-
-
-// var participants = $("#participants option:selected").val();
-// var badWeather = $('#weather4').text(); 
-// // var maxprice = $("#maxprice option:selected").val();
-//   console.log(participants);
-// if (weatherdesc == "rain"){
-//     fetch('http://www.boredapi.com/api/activity?&participants='+participants+'&type=education')
-//     .then(function (response) {
-//     return response.json();
-//     })
-//     .then(function(data){
-//         console.log(data)
-//         var act = data.activity;
-//         var participants = data.participants
-//         var price = data.price;
-//         $('#activity1').text(act);
-//         $('#activity2').text('as far as price, we rate this as a '+ price+' on a scale of 0 - 1')
-//         $('#activity3').text('this is an idea for an activity with '+participants+' participant(s). run the code again if you dont like it.');
-
-//     })
-// }else if (weatherdesc !== "rain"){
-//     fetch('http://www.boredapi.com/api/activity?&participants='+participants+'&type=recreational')
-//     .then(function (response) {
-//     return response.json();
-//     })
-//     .then(function(data){
-//         console.log(data)
-//         var act = data.activity;
-//         var participants = data.participants
-//         var price = data.price;
-//         $('#activity1').text(act);
-//         $('#activity2').text('as far as price, we rate this as a '+ price +' on a scale of 0 - 1')
-//         $('#activity3').text(participants);
-
-//     })
-// }
-// })
-// }
-// $('#submit').on('click',boredAPI)
